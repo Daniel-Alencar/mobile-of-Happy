ffdbbb63d94eadbcfa7007d7ec6826b95b9c0960
Daniel-Alencar <danielalencar746@gmail.com>
2020-11-26 11:24:24 -0300

    adicionado ícone map-marker

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
 
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}/>
+      <MapView 
+        provider={PROVIDER_GOOGLE}
+        style={styles.map} 
+        initialRegion={{
+          latitude: -7.5715255,
+          longitude: -40.5214804,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.015,
+        }}
+      >
+        <Marker 
+          icon={mapMarker}
+          coordinate={{
+            latitude: -7.5715255,
+            longitude: -40.5214804,
+          }}
+        />
+      </MapView>
     </View>
   );
 }
@@ -15,6 +34,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
