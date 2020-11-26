92426fb8328b1859e978e290ebaf3a5bf5302672
Daniel-Alencar <danielalencar746@gmail.com>
2020-11-26 11:57:54 -0300

    feito o footer

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE, Callout } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
 
 import mapMarker from './src/images/map-marker.png';
 
@@ -20,12 +21,33 @@ export default function App() {
       >
         <Marker 
           icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8,
+          }}
           coordinate={{
             latitude: -7.5715255,
             longitude: -40.5214804,
           }}
-        />
+        >
+          <Callout tooltip={true} onPress={() => {}}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>
+                Lar das meninas
+              </Text>
+            </View>
+          </Callout>
+         </Marker>
       </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+          <Feather name="plus" size={20} color="#fff">
+
+          </Feather>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -38,5 +60,50 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    height: 46,
+    width: 160,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
+  },
+
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 24,
+
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    elevation: 4
+  },
+
+  footerText: {
+    color: '#8fa7b3',
+  },
+
+  createOrphanageButton: {
+    backgroundColor: '#15c3d6',
+    width: 56,
+    height: 56,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
